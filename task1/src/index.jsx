70f5728398b7e8fd2cc3fd1326e019d432a5db4a
Dima Bondariev <86620950+dbondariev@users.noreply.github.com>
2022-08-05 10:29:59 +0300

    Delete task1 directory

--- task1/src/index.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import Auth from './Auth';
-
-const rootElement = document.querySelector('#root');
-
-ReactDOM.render(<Auth />, rootElement);
