1ce457fc6f4130e5d197ed0b959da7387850876f
dbondariev <dima.bondariev@gmail.com>
2022-08-20 01:44:39 +0300

    lesson14 homework

--- lesson14/task5/src/index.jsx
+++
@@ -1,8 +0,0 @@
-import ReactDOM from "react-dom";
-import React from "react";
-import "./index.scss";
-import App from "./App.jsx";
-
-const rootElem = document.querySelector("#root");
-
-ReactDOM.render(<App />, rootElem);
